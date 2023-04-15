b8a556f790d32a8968e7584e0a5623c8be6d14d5
manojigit <130819027+manojigit@users.noreply.github.com>
2023-04-15 13:16:04 +0530

    second test added

---
+++ src/test/java/com/testcases/GetAPISecondTest.java
@@ -0,0 +1,23 @@
+package com.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class GetAPISecondTest {
+
+	
+	@Test
+	public void testCase2() {
+		//https://reqres.in/api/users/2
+		RequestSpecification requestSpec = RestAssured.given().contentType("application/json");
+		Response response = requestSpec.get("https://reqres.in/api/users/2");
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getBody().asPrettyString());
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+	}
+}
